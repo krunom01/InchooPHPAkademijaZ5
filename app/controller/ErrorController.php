316359ad6006357599dd25452dc38c0a33e512ca
krunom01 <krunom92@gmail.com>
2019-01-30 22:53:02 +0100

    trying to fix problem with new Error controller

---
+++ app/controller/ErrorController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kruno
+ * Date: 30/01/19
+ * Time: 18.52
+ */
+
+class ErrorController
+{
+    public function __construct()
+    {
+        echo "404 error";
+    }
+}
